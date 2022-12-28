9649247d2cec5e3d61c8bb3d5fd67e4aaf7a14ff
lindaperea <linda.hp@hotmail.com>
2022-12-27 19:08:53 -0500

    firstCommit

---
+++ src/CardsData.jsx
@@ -0,0 +1,88 @@
+// id
+//incon: "fa-solid fa-house"
+// status: "up | "down" | "selected"
+
+const cardsData = [
+    {
+        id: 1,
+        icon: "fa-solid fa-house",
+        status: "down"
+    },
+    {
+        id: 2,
+        icon: "fa-solid fa-house",
+        status: "down"
+    },
+     {
+        id: 3,
+        icon: "fa-solid fa-hippo",
+        status: "down"
+    },
+    {
+        id: 4,
+        icon: "fa-solid fa-hippo",
+        status: "down"
+    },
+     {
+        id: 5,
+        icon: "fa-solid fa-gift",
+        status: "down"
+    },
+    {
+        id: 6,
+        icon: "fa-solid fa-gift",
+        status: "down"
+    },
+     {
+        id: 7,
+        icon: "fa-solid fa-snowflake",
+        status: "down"
+    },
+    {
+        id: 8,
+        icon: "fa-solid fa-snowflake",
+        status: "down"
+    },
+     {
+        id: 9,
+        icon: "fa-solid fa-heart",
+        status: "down"
+    },
+    {
+        id: 10,
+        icon: "fa-solid fa-heart",
+        status: "down"
+    },
+     {
+        id: 11,
+        icon: "fa-solid fa-truck-medical",
+        status: "down"
+    },
+    {
+        id: 12,
+        icon: "fa-solid fa-truck-medical",
+        status: "down"
+    },
+     {
+        id: 13,
+        icon: "fa-solid fa-snowman",
+        status: "down"
+    },
+    {
+        id: 14,
+        icon: "fa-solid fa-snowman",
+        status: "down"
+    },
+     {
+        id: 15,
+        icon: "fa-solid fa-motorcycle",
+        status: "down"
+    },
+    {
+        id: 16,
+        icon: "fa-solid fa-motorcycle",
+        status: "down"
+    }
+]
+
+export default cardsData;
